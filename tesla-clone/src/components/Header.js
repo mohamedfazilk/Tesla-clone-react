9f4f961352b842ed2d15406ddbf14c033a00c2b5
mohamedfazilk <2114921@cognizant.com>
2022-11-08 16:31:30 +0530

    create the border for the Header

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <a><img src="images/logo.svg"/></a>
+
+      <Menu>
+        <p><a href="#">Model S</a></p>
+        <p><a href="#">Model 3</a></p>
+        <p><a href="#">Model X</a></p>
+        <p><a href="#">Model Y</a></p>
+        <p><a href="#">Shop</a></p>
+        <p><a href="#">Tesla Account</a></p>
+
+      </Menu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+padding:0 20px;
+`
+const Menu = styled.div`
+
+`
