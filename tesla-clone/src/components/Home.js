f045f42cc5d71c8eab06cd9e24b05e2ff964ab71
mohamedfazilk <2114921@cognizant.com>
2022-11-09 12:21:32 +0530

    create the useState for closing and opening the menu

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -1,74 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
-import Section from './Section'
-
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
 function Home() {
   return (
     <Container>
       <Section
-      title="Model S"
-      description="Order Online for Touch Delivery"
-      backgroundImg= "model-s.jpg"
-      leftBtnText= "Custom Order"
-      rightBtnText = "Existing Inventory"
+        title="Model S"
+        description="Order Online for Touch Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
       />
-  <Section
-      title="Model Y"
-      description="Order Online for Touch Delivery"
-      backgroundImg= "model-y.jpg"
-      leftBtnText= "Custom Order"
-      rightBtnText = "Existing Inventory"
-      />
-
-<Section
-      title="Model 3"
-      description="Order Online for Touch Delivery"
-      backgroundImg= "model-3.jpg"
-      leftBtnText= "Custom Order"
-      rightBtnText = "Existing Inventory"
+      <Section
+        title="Model Y"
+        description="Order Online for Touch Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
       />
 
-<Section
-      title="Model  X"
-      description="Order Online for Touch Delivery"
-      backgroundImg= "model-x.jpg"
-      leftBtnText= "Custom Order"
-      rightBtnText = "Existing Inventory"
+      <Section
+        title="Model 3"
+        description="Order Online for Touch Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
       />
 
-<Section
-      title="Lowest Cost Solar Panels in America"
-      description="money-back guarantee"
-      backgroundImg= "solar-panel.jpg"
-      leftBtnText= "Order Now"
-      rightBtnText = "Learn More"
+      <Section
+        title="Model  X"
+        description="Order Online for Touch Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
       />
 
-      
-<Section
-      title="Solar For New Roofs"
-      description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
-      backgroundImg= "solar-roof.jpg"
-      leftBtnText= "Order Now"
-      rightBtnText = "Learn More"
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
       />
-            
-<Section
-      title="Accessories"
-      description=""
-      backgroundImg= "accessories.jpg"
-      leftBtnText= "Shop Now"
-   
-      />
-      
 
-      </Container>
-  )
+      <Section
+        title="Solar For New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-height: 100vh;
-`
+  height: 100vh;
+`;
