988cdd69de0c36390737b966df0e5a3a3a11ac14
mohamedfazilk <2114921@cognizant.com>
2022-11-09 16:17:05 +0530

    add the Header component

---
+++ disney-plus-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+   <Header/>
+    </div>
+  );
+}
+
+export default App;
