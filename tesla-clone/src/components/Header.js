818df781d913b0663b4b4ae108a7981c09781cca
mohamedfazilk <2114921@cognizant.com>
2022-11-09 10:49:39 +0530

    place the MenuIcon from material Ui

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
   return (
@@ -7,14 +8,18 @@ function Header() {
       <a><img src="images/logo.svg"/></a>
 
       <Menu>
-        <p><a href="#">Model S</a></p>
-        <p><a href="#">Model 3</a></p>
-        <p><a href="#">Model X</a></p>
-        <p><a href="#">Model Y</a></p>
-        <p><a href="#">Shop</a></p>
-        <p><a href="#">Tesla Account</a></p>
+      <a href="#">Model S</a>
+      <a href="#">Model 3</a>
+      <a href="#">Model X</a>
+      <a href="#">Model Y</a>
+       
 
       </Menu>
+      <RightMenu>
+      <a href="#">Shop</a>
+      <a href="#">Tesla Account</a>
+      <CustomMenu/> 
+      </RightMenu>
     </Container>
   )
 }
@@ -27,7 +32,34 @@ position:fixed;
 display:flex;
 align-items:center;
 padding:0 20px;
+top:0;
+left:0;
+right:0;
 `
 const Menu = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+flex:1;
 
+a{
+  font-weight:600;
+  text-transform:uppercase;
+  padding:0  10px;
+  flex-wrap:nowrap;
+  
+}
 `
+
+const RightMenu = styled.div`
+
+a{
+  font-weight:600;
+  text-transform:uppercase;
+margin-right:10px;
+  
+}
+`
+
+const CustomMenu = styled(MenuIcon)``
+
